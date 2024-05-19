5f187d3294b7491d9555d29cdf74ed76d8a9638c
kien <kien@gmailcom>
2024-05-18 22:09:48 -0700

    abc test

---
+++ src/java/entity/Dog.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class Dog {
+    String name ;
+
+    public Dog() {
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+    
+}
